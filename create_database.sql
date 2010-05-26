7e0101de0ced141d2084cb7c585d4931f0b53479
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-05-26 12:39:04 -0400

    Forgot the votes in DB

--- create_database.sql
+++ create_database.sql
@@ -27,6 +27,7 @@ CREATE TABLE `candidates` (
   `name` char(255) NOT NULL,
   `position_id` int(11) NOT NULL,
   `image_url` char(255) NOT NULL,
+  `votes` int(11),
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
